6289b7090e0a8abf4828fab60b8fb68e49395f7d
piaoger gong <piaoger@mymaclocal.local>
2021-07-13 15:11:19 +0800

    feat:add benchmarks

--- README.md
+++ README.md
@@ -1,2 +1,98 @@
-# gjson-bench
-benchmarks of gjson implementations in rust
+
+## gson benchmark
+
+It's a fork of ajson's benches to focus on perfmance benchmarks only.
+
+## Performance
+
+`$ cargo bench`
+
+* [ajson](https://github.com/importcjj/ajson)
+* [gjson](https://github.com/tidwall/gjson.rs)
+* [serde_json](https://github.com/serde-rs/json)
+* [rust-json](https://github.com/maciejhirsz/json-rust)
+
+```
+ajson benchmark         time:   [9.1979 us 9.2489 us 9.3066 us]
+                        change: [-10.706% -9.1719% -7.6992%] (p = 0.00 < 0.05)
+                        Performance has improved.
+Found 2 outliers among 100 measurements (2.00%)
+  2 (2.00%) high mild
+
+gjson benchmark         time:   [3.6221 us 3.6413 us 3.6623 us]
+                        change: [-15.350% -12.966% -10.873%] (p = 0.00 < 0.05)
+                        Performance has improved.
+Found 6 outliers among 100 measurements (6.00%)
+  3 (3.00%) high mild
+  3 (3.00%) high severe
+
+serde_json benchmark    time:   [59.616 us 59.876 us 60.162 us]
+                        change: [-14.793% -12.771% -10.900%] (p = 0.00 < 0.05)
+                        Performance has improved.
+Found 9 outliers among 100 measurements (9.00%)
+  1 (1.00%) low mild
+  2 (2.00%) high mild
+  6 (6.00%) high severe
+
+json-rust benchmark     time:   [29.568 us 29.729 us 29.894 us]
+                        change: [-15.512% -14.014% -12.561%] (p = 0.00 < 0.05)
+                        Performance has improved.
+Found 5 outliers among 100 measurements (5.00%)
+  1 (1.00%) low mild
+  3 (3.00%) high mild
+  1 (1.00%) high severe
+
+ajson selector          time:   [3.0875 us 3.1000 us 3.1139 us]
+                        change: [-11.651% -10.079% -8.4826%] (p = 0.00 < 0.05)
+                        Performance has improved.
+Found 5 outliers among 100 measurements (5.00%)
+  5 (5.00%) high mild
+
+gjson selector          time:   [2.0724 us 2.0882 us 2.1050 us]
+                        change: [-5.7071% -3.8407% -1.9520%] (p = 0.00 < 0.05)
+                        Performance has improved.
+Found 4 outliers among 100 measurements (4.00%)
+  4 (4.00%) high severe
+
+ajson multi query       time:   [2.6245 us 2.6365 us 2.6492 us]
+                        change: [-9.4889% -8.3135% -7.1778%] (p = 0.00 < 0.05)
+                        Performance has improved.
+Found 9 outliers among 100 measurements (9.00%)
+  1 (1.00%) low mild
+  5 (5.00%) high mild
+  3 (3.00%) high severe
+
+gjson multi query       time:   [858.23 ns 861.61 ns 865.37 ns]
+                        change: [-10.479% -8.6397% -6.8407%] (p = 0.00 < 0.05)
+                        Performance has improved.
+Found 5 outliers among 100 measurements (5.00%)
+  3 (3.00%) high mild
+  2 (2.00%) high severe
+
+serde derive            time:   [9.1830 us 9.2368 us 9.2964 us]
+                        change: [-10.407% -8.5730% -6.7286%] (p = 0.00 < 0.05)
+                        Performance has improved.
+Found 5 outliers among 100 measurements (5.00%)
+  4 (4.00%) high mild
+  1 (1.00%) high severe
+
+serde derive multi query
+                        time:   [1.9318 us 1.9421 us 1.9542 us]
+                        change: [-5.3005% -4.0650% -2.8234%] (p = 0.00 < 0.05)
+                        Performance has improved.
+Found 3 outliers among 100 measurements (3.00%)
+  3 (3.00%) high mild
+
+nom json bench          time:   [1.2689 us 1.2735 us 1.2783 us]
+                        change: [-4.7747% -3.7796% -2.8171%] (p = 0.00 < 0.05)
+                        Performance has improved.
+Found 5 outliers among 100 measurements (5.00%)
+  2 (2.00%) high mild
+  3 (3.00%) high severe
+
+
+```
+
+* MacBook Pro (Retina, 15-inch, Mid 2015)
+* 2.2 GHz Quad-Core Intel Core i7
+* 16 GB 1600 MHz DDR3
---
+++ benches/ajson_benchmark.rs
@@ -0,0 +1,355 @@
+#[macro_use]
+extern crate criterion;
+
+use criterion::black_box;
+use criterion::Criterion;
+extern crate ajson;
+extern crate json;
+
+#[macro_use]
+extern crate serde;
+extern crate nom;
+extern crate serde_json;
+mod nom_json;
+use nom::error::ErrorKind;
+
+use json::JsonValue;
+use serde_json::Value;
+
+#[allow(dead_code)]
+static BENCH_DATA: &'static str = r#"{
+    "overflow": 9223372036854775808,
+    "widget": {
+        "debug": "on",
+        "window": {
+            "title": "Sample Konfabulator Widget",
+            "name": "main_window",
+            "width": 500,
+            "height": 500
+        },
+        "image": {
+            "src": "Images/Sun.png",
+            "hOffset": 250,
+            "vOffset": 250,
+            "alignment": "center"
+        },
+        "text": {
+            "data": "Click Here",
+            "size": 36,
+            "style": "bold",
+            "vOffset": 100,
+            "alignment": "center",
+            "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
+        },
+        "menu": [
+            {
+                "title": "file",
+                "sub_item": 7,
+                "options": [1, 2, 3]
+            },
+            {
+                "title": "edit",
+                "sub_item": 14,
+                "options": [4, 5]
+            },
+            {
+                "title": "help",
+                "sub_item": 4,
+                "options": [6, 7, 8]
+            }
+        ]
+    }
+}"#;
+
+
+
+fn ajson_selector(json: &str) {
+    black_box(
+        ajson::get(json, "widget.[image.src,text.data]")
+            .unwrap()
+            .to_vec(),
+    );
+}
+
+fn ajson_multi_query(json: &str) {
+    black_box([
+        ajson::get(json, "widget.image.src"),
+        ajson::get(json, "widget.text.data"),
+    ]);
+}
+
+fn ajson_bench(json: &str) {
+    black_box(ajson::get(json, "widget.window.name").unwrap().as_str());
+    black_box(ajson::get(json, "widget.image.hOffset").unwrap().to_f64());
+    black_box(ajson::get(json, "widget.text.onMouseUp").unwrap().as_str());
+    black_box(ajson::get(json, "widget.debug").unwrap().as_str());
+    // ajson::get(json, "widget.text").as_map();
+    black_box(
+        ajson::get(json, "widget.menu.#(sub_item>7)#.title")
+            .unwrap()
+            .to_vec(),
+    );
+    // ajson::get(json, "widget.menu.[1.title,2.options]").as_array();
+}
+
+
+fn gjson_selector(json: &str) {
+    black_box(
+        gjson::get(json, "widget.[image.src,text.data]")
+            .array(),
+    );
+}
+
+fn gjson_multi_query(json: &str) {
+    black_box([
+        gjson::get(json, "widget.image.src"),
+        gjson::get(json, "widget.text.data"),
+    ]);
+}
+
+fn gjson_bench(json: &str) {
+    black_box(gjson::get(json, "widget.window.name").str());
+    black_box(gjson::get(json, "widget.image.hOffset").f64());
+    black_box(gjson::get(json, "widget.text.onMouseUp").str());
+    black_box(gjson::get(json, "widget.debug").str());
+    // ajson::get(json, "widget.text").as_map();
+    black_box(
+        gjson::get(json, "widget.menu.#(sub_item>7)#.title")
+            .array(),
+    );
+    // gjson::get(json, "widget.menu.[1.title,2.options]").as_array();
+}
+
+
+fn json_rust_bench(data: &str) {
+    let a = &json::parse(data).unwrap();
+    black_box(a["widget"]["window"]["name"].as_str().unwrap());
+    let b = &json::parse(data).unwrap();
+    black_box(b["widget"]["image"]["hOffset"].as_i64().unwrap());
+    let c = &json::parse(data).unwrap();
+    black_box(c["widget"]["text"]["onMouseUp"].as_str().unwrap());
+    let d = &json::parse(data).unwrap();
+    black_box(d["widget"]["debug"].as_str().unwrap());
+
+    // let text = &serde_json::from_str::<Value>(BENCH_DATA).unwrap()["widget"]["text"] ;
+
+    let menu = &json::parse(data).unwrap()["widget"]["menu"];
+    let _v: Vec<&JsonValue> = black_box(menu
+        .members()
+        .filter(|x| x["sub_item"].as_i64().unwrap() > 5)
+        .map(|x| &x["title"])
+        .collect());
+}
+
+fn serde_json_bench(json: &str) {
+    let a = &serde_json::from_str::<Value>(json).unwrap();
+    black_box(a["widget"]["window"]["name"].as_str().unwrap());
+    let b = &serde_json::from_str::<Value>(json).unwrap();
+    black_box(b["widget"]["image"]["hOffset"].as_i64().unwrap());
+    let c = &serde_json::from_str::<Value>(json).unwrap();
+    black_box(c["widget"]["text"]["onMouseUp"].as_str().unwrap());
+    let d = &serde_json::from_str::<Value>(json).unwrap();
+    black_box(d["widget"]["debug"].as_str().unwrap());
+
+    // // let text = &serde_json::from_str::<Value>(BENCH_DATA).unwrap()["widget"]["text"] ;
+
+    let menu = &serde_json::from_str::<Value>(json).unwrap();
+
+    let _v: Vec<&Value> = black_box(
+        menu["widget"]["menu"]
+            .as_array()
+            .unwrap()
+            .iter()
+            .filter(|x| x["sub_item"].as_i64().unwrap() > 5)
+            .map(|x| &x["title"])
+            .collect(),
+    );
+}
+
+fn serde_json_derive_bench(json: &str) {
+    #![allow(non_snake_case)]
+
+    {
+        #[derive(Deserialize)]
+        struct Main {
+            widget: Widget,
+        }
+        #[derive(Deserialize)]
+        struct Widget {
+            window: Window,
+        }
+        #[derive(Deserialize)]
+        struct Window {
+            name: String,
+        }
+
+        let a = serde_json::from_str::<Main>(json).unwrap();
+        black_box(a.widget.window.name);
+    }
+
+    {
+        #[derive(Deserialize)]
+        struct Main {
+            widget: Widget,
+        }
+        #[derive(Deserialize)]
+        struct Widget {
+            image: Image,
+        }
+        #[derive(Deserialize)]
+        struct Image {
+            hOffset: i64,
+        }
+        let b = serde_json::from_str::<Main>(json).unwrap();
+        black_box(b.widget.image.hOffset);
+    }
+
+    {
+        #[derive(Deserialize)]
+        struct Main {
+            widget: Widget,
+        }
+        #[derive(Deserialize)]
+        struct Widget {
+            text: Text,
+        }
+        #[derive(Deserialize)]
+        struct Text {
+            onMouseUp: String,
+        }
+        let c = serde_json::from_str::<Main>(json).unwrap();
+        black_box(c.widget.text.onMouseUp);
+    }
+
+    {
+        #[derive(Deserialize)]
+        struct Main {
+            widget: Widget,
+        }
+        #[derive(Deserialize)]
+        struct Widget {
+            debug: String,
+        }
+        let d = serde_json::from_str::<Main>(json).unwrap();
+        black_box(d.widget.debug);
+    }
+
+    {
+        #[derive(Deserialize)]
+        struct Main {
+            widget: Widget,
+        }
+        #[derive(Deserialize)]
+        struct Widget {
+            menu: Vec<Item>,
+        }
+        #[derive(Deserialize)]
+        struct Item {
+            sub_item: i64,
+            title: Value,
+        }
+        let e = serde_json::from_str::<Main>(json).unwrap();
+        black_box(
+            e.widget
+                .menu
+                .into_iter()
+                .filter(|x| x.sub_item > 5)
+                .map(|x| x.title)
+                .collect::<Vec<_>>(),
+        );
+    }
+}
+
+fn nom_json_bench(json: &str) {
+    match nom_json::root::<(&str, ErrorKind)>(json) {
+        Ok((_, value)) => {
+            black_box(&value["widget"]["window"]["name"].as_str());
+            black_box(&value["widget"]["image"]["hOffset"]);
+            black_box(&value["widget"]["text"]["onMouseUp"].as_str());
+            black_box(&value["widget"]["debug"].as_str());
+
+            let menu = &value["widget"]["menu"];
+            let _v: Vec<&nom_json::JsonValue> = black_box(
+                menu.members()
+                    .filter(|x| x["sub_item"].to_f64() > 5.0)
+                    .map(|x| &x["title"])
+                    .collect(),
+            );
+        }
+        _ => (),
+    };
+}
+
+fn serde_json_derive_multi_query(json: &str) {
+    #[derive(Deserialize)]
+    struct Main {
+        widget: Widget,
+    }
+    #[derive(Deserialize)]
+    struct Widget {
+        image: Image,
+        text: Text,
+    }
+    #[derive(Deserialize)]
+    struct Image {
+        src: Value,
+    }
+    #[derive(Deserialize)]
+    struct Text {
+        data: Value,
+    }
+
+    let a = serde_json::from_str::<Main>(json).unwrap();
+    black_box([a.widget.image.src, a.widget.text.data]);
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    // c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+
+    c.bench_function("ajson benchmark", |b| {
+        b.iter(|| ajson_bench(black_box(BENCH_DATA)))
+    });
+
+    c.bench_function("gjson benchmark", |b| {
+        b.iter(|| gjson_bench(black_box(BENCH_DATA)))
+    });
+
+    c.bench_function("serde_json benchmark", |b| {
+        b.iter(|| serde_json_bench(black_box(BENCH_DATA)))
+    });
+
+    c.bench_function("json-rust benchmark", |b| {
+        b.iter(|| json_rust_bench(black_box(BENCH_DATA)))
+    });
+
+    c.bench_function("ajson selector", |b| {
+        b.iter(|| ajson_selector(black_box(BENCH_DATA)))
+    });
+
+    c.bench_function("gjson selector", |b| {
+        b.iter(|| gjson_selector(black_box(BENCH_DATA)))
+    });
+
+    c.bench_function("ajson multi query", |b| {
+        b.iter(|| ajson_multi_query(black_box(BENCH_DATA)))
+    });
+
+    c.bench_function("gjson multi query", |b| {
+        b.iter(|| gjson_multi_query(black_box(BENCH_DATA)))
+    });
+
+
+    c.bench_function("serde derive", |b| {
+        b.iter(|| serde_json_derive_bench(black_box(BENCH_DATA)))
+    });
+    c.bench_function("serde derive multi query", |b| {
+        b.iter(|| serde_json_derive_multi_query(black_box(BENCH_DATA)))
+    });
+
+    c.bench_function("nom json bench", |b| {
+        b.iter(|| nom_json_bench(black_box(BENCH_DATA)))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
